e90926eed8bd1fbebdf66f331d107d9e090e41a3
sunghwan <jeonsung02@gmail.com>
2020-01-22 11:06:14 +0900

    항상 1000001까지 만들던 것을 N까지만 만들도록 수정

--- make_one.py
+++ make_one.py
@@ -1,9 +1,9 @@
 N = int(input())
 cnt = 0
 
-table = [0] * 1000001
+table = [0] * (N+1)
 
-for i in range(2, 1000001):
+for i in range(2, N+1):
     if i % 6 == 0:
         table[i] = min(table[i//2], table[i//3], table[i-1]) + 1
         continue
