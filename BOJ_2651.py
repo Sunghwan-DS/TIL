ad4a0518e7af710416711ce24999a2c54dbc605a
Sunghwan-DS <jeonsung02@gmail.com>
2020-02-03 00:07:36 +0900

    시간 매번 계산하던거 삭제 but 시간초과

--- BOJ_2651.py
+++ BOJ_2651.py
@@ -3,31 +3,33 @@ N = int(input())
 distance = list(map(int,input().split()))   # N+1개의 자료
 time = list(map(int,input().split()))      # N+1개의 자료
 
-queue = [[0,B,0,[]]]   # 시간, 갈수있는 거리, n번째 장소, 들른 주유소
+queue = [[B,0,[]]]   # 갈수있는 거리, n번째 장소, 들른 주유소
 
 # print(time)
 # print(distance)
 result = []
-limit_time = [[] for _ in range(N)]
 if B >= sum(distance):
     print("0")
     print("0")
     exit()
 while queue:
     a = queue.pop(0)
-    if a[2] == N:
-        if a[1] - distance[a[2]] >= 0:
-            result.append(a[0])
-            if a[0] == min(result):
-                lst = a[3]
+    if a[1] == N:
+        if a[0] - distance[a[1]] >= 0:
+            t = 0
+            for __ in a[2]:
+                t += time[__-1]
+            result.append(t)
+            if t == min(result):
+                lst = a[2]
             continue
     # if a[0] > limit_time[n]:
     #     pass
-    if a[1] - distance[a[2]] >= 0:
-        queue.append([a[0], a[1]-distance[a[2]], a[2]+1, a[3]])
-        next = a[3][:]
-        next.append(a[2]+1)
-        queue.append([a[0] + time[a[2]], B, a[2]+1, next])
+    if a[0] - distance[a[1]] >= 0:
+        queue.append([a[0]-distance[a[1]], a[1]+1, a[2]])
+        next = a[2][:]
+        next.append(a[1]+1)
+        queue.append([B, a[1]+1, next])
 
 print(min(result))
 print(len(lst))
