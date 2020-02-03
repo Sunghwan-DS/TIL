9966e854e3cc8842e2a10a671f4def858a8a06d3
sunghwan <jeonsung02@gmail.com>
2020-02-03 10:26:30 +0900

    정비를 받은 정비소에 대해 시간이 가장 짧은 경우만 queue에 추가, but 런타임 에러

--- BOJ_2651.py
+++ BOJ_2651.py
@@ -5,31 +5,44 @@ time = list(map(int,input().split()))      # N+1개의 자료
 
 queue = [[B,0,[]]]   # 갈수있는 거리, n번째 장소, 들른 주유소
 
-# print(time)
-# print(distance)
 result = []
 if B >= sum(distance):
     print("0")
     print("0")
+    print("")
     exit()
+
 while queue:
-    a = queue.pop(0)
-    if a[1] == N:
+    size = len(queue)
+    test = []
+    for i in range(size):
+        a = queue.pop(0)
+        if a[1] == N:
+            if a[0] - distance[a[1]] >= 0:
+                t = 0
+                for __ in a[2]:
+                    t += time[__-1]
+                result.append(t)
+                if t == min(result):
+                    lst = a[2]
+                continue
+
         if a[0] - distance[a[1]] >= 0:
-            t = 0
-            for __ in a[2]:
-                t += time[__-1]
-            result.append(t)
-            if t == min(result):
-                lst = a[2]
-            continue
-    # if a[0] > limit_time[n]:
-    #     pass
-    if a[0] - distance[a[1]] >= 0:
-        queue.append([a[0]-distance[a[1]], a[1]+1, a[2]])
-        next = a[2][:]
-        next.append(a[1]+1)
-        queue.append([B, a[1]+1, next])
+            queue.append([a[0]-distance[a[1]], a[1]+1, a[2]])
+            next = a[2][:]
+            next.append(a[1]+1)
+            test.append([B, a[1]+1, next])
+
+    t_lst = []
+    for j in test:
+        t = 0
+        for __ in j[2]:
+            t += time[__ - 1]
+            t_lst.append(t)
+    if t_lst == []:
+        break
+    b = t_lst.index(min(t_lst))
+    queue.append(test[b])
 
 print(min(result))
 print(len(lst))
