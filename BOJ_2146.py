e7aefc341f46f4e062add944373bd44545e42dee
sunghwan <jeonsung02@gmail.com>
2020-02-04 16:12:33 +0900

    2020.02.04 해결

--- BOJ_2146.py
+++ BOJ_2146.py
@@ -2,9 +2,10 @@ def numbering(i, j, cnt):
     dy = [-1, 0, 1, 0]
     dx = [0, 1, 0, -1]
     queue = [[i, j]]
+    edge_lst = []
 
     while queue:
-        lst = queue.pop(0)
+        lst = queue.pop(-1)
         y = lst[0]
         x = lst[1]
         field[y][x] = cnt
@@ -12,41 +13,48 @@ def numbering(i, j, cnt):
             if 0 <= y+dy[dir] <= N-1 and 0 <= x+dx[dir] <= N-1:
                 if field[y+dy[dir]][x+dx[dir]] == 1:
                     queue.append([y+dy[dir], x+dx[dir]])
+                elif field[y+dy[dir]][x+dx[dir]] == 0:
+                    if [y,x] not in edge_lst:
+                        edge.append([y,x])
 
-def BFS(y, x, value):
+
+def BFS():
     global result
-    queue = [[y, x]]
+    queue = edge[:]
     dy = [-1, 0, 1, 0]
     dx = [0, 1, 0, -1]
     cnt = 0
-    new_field = []
-    for f in field:
-        new_field.append(f[:])
+    result = 9999
+
     while queue:
         for num in range(len(queue)):
             lst = queue.pop(0)
             y = lst[0]
             x = lst[1]
+            value = field[y][x]
             for dir in range(4):
                 if 0 <= y + dy[dir] <= N - 1 and 0 <= x + dx[dir] <= N - 1:
-                    if new_field[y + dy[dir]][x + dx[dir]] == value:
+                    if field[y + dy[dir]][x + dx[dir]] == value:
                         pass
-                    elif new_field[y + dy[dir]][x + dx[dir]] == 0:
-                        new_field[y + dy[dir]][x + dx[dir]] = value
+                    elif field[y + dy[dir]][x + dx[dir]] == 0:
+                        field[y + dy[dir]][x + dx[dir]] = value
                         queue.append([y + dy[dir], x + dx[dir]])
-                    elif new_field[y + dy[dir]][x + dx[dir]] != 0:
-                        if cnt < result:
-                            result = cnt
-                            return
+                    elif field[y + dy[dir]][x + dx[dir]] != 0:
+                        if field[y + dy[dir]][x + dx[dir]] > value:
+                            if result > cnt*2:
+                                result = cnt*2
+                        else:
+                            if result > cnt*2+1:
+                                result = cnt*2+1
+        if result != 9999:
+            return result
         cnt += 1
-        if cnt == result:
-            return
 
 N = int(input())
 field = []
 for _ in range(N):
     field.append(list(map(int,input().split())))
-
+edge = []
 cnt = 1
 for i in range(N):
     for j in range(N):
@@ -54,10 +62,5 @@ for i in range(N):
             cnt += 1
             numbering(i, j, cnt)
 
-result = 200
-for i in range(N):
-    for j in range(N):
-        if field[i][j] != 0:
-            BFS(i, j, field[i][j])
 
-print(result)
+print(BFS())
