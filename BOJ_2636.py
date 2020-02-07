695019321e6d112ba3462b67c1dc114d6e6f2e16
Sunghwan-DS <jeonsung02@gmail.com>
2020-02-08 01:51:08 +0900

    2020.02.08

--- BOJ_2636.py
+++ BOJ_2636.py
@@ -1,31 +1,19 @@
 def air(i, j):
-    queue = [[i, j]]
-    while queue:
-        current = queue.pop(-1)
-        y = current[0]
-        x = current[1]
-        arr[y][x] = 9
-        for d in range(4):
-            if 0 <= y + dy[d] <= N - 1 and 0 <= x + dx[d] <= M - 1:
-                if arr[y + dy[d]][x + dx[d]] == 0:
-                    queue.append([y + dy[d], x + dx[d]])
-
-def new_air(i, j):
-    global queue
     q = [[i, j]]
-    new = []
     while q:
         current = q.pop(-1)
         y = current[0]
         x = current[1]
         arr[y][x] = 9
-        new.append([y, x])
         for d in range(4):
-            if 0 <= y + dy[d] <= N - 1 and 0 <= x + dx[d] <= M - 1:
-                if arr[y + dy[d]][x + dx[d]] == 0:
-                    q.append([y + dy[d], x + dx[d]])
-    print("뉴!!!", new)
-    queue += new
+            ny = y + dy[d]
+            nx = x + dx[d]
+            if 0 <= ny <= N - 1 and 0 <= nx <= M - 1:
+                if arr[ny][nx] == 0:
+                    q.append([ny, nx])
+                elif arr[ny][nx] == 1:
+                    if [ny, nx] not in queue:
+                        queue.append([ny, nx])
 
 
 N, M = map(int,input().split())
@@ -36,44 +24,32 @@ for i in range(N):
 dy = [0, 1, 0, -1]
 dx = [1, 0, -1, 0]
 
+queue = []
 air(0, 0)
 
-for __ in range(N):
-    print(arr[__])
-print()
-
-queue = []
-
-for i in range(N):
-    for j in range(M):
-        if arr[i][j] == 1:
-            for d in range(4):
-                if 0 <= i + dy[d] <= N - 1 and 0 <= j + dx[d] <= N - 1:
-                    if arr[i + dy[d]][j + dx[d]] == 9:
-                        queue.append([i, j])
-                        break
-
-# print(queue)
+if queue == []:
+    print(0)
+    print(0)
+    exit()
 
 cnt = 0
 while queue:
     last = len(queue)
     for _ in range(len(queue)):
         current = queue.pop(0)
-        i = current[0]
-        j = current[1]
-        arr[i][j] = 9
+        y = current[0]
+        x = current[1]
+        arr[y][x] = 9
         for d in range(4):
-            if 0 <= i + dy[d] <= N - 1 and 0 <= j + dx[d] <= N - 1:
-                if arr[i + dy[d]][j + dx[d]] == 1:
-                    queue.append([i + dy[d], j + dx[d]])
-                elif arr[i + dy[d]][j + dx[d]] == 0:
-                    new_air(i + dy[d], j + dx[d])
-
-    # for __ in range(N):
-    #     print(arr[__])
-    # print()
-    # cnt += 1
+            ny = y + dy[d]
+            nx = x + dx[d]
+            if 0 <= ny <= N - 1 and 0 <= nx <= M - 1:
+                if arr[ny][nx] == 1:
+                    if [ny, nx] not in queue:
+                        queue.append([ny, nx])
+                elif arr[ny][nx] == 0:
+                    air(ny, nx)
+    cnt += 1
 
 print(cnt)
 print(last)
