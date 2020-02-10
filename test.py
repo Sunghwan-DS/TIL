6cad8a367d414dbdc84b8b38b6b421c6b5ae5823
sunghwan <jeonsung02@gmail.com>
2020-02-10 18:01:10 +0900

    2020.02.10 해결

--- test.py
+++ test.py
@@ -1,17 +1,38 @@
-num = int(input())
-data = [[i for i in map(int,input().split())] for j in range(num)]  # num 개의 색종이 자료
-mymap = [[0]*101 for i in range(101)]   # 101*101
-for i in range(num):
-    for j in range(data[i][0], data[i][0] + data[i][2]):
-        for k in range(data[i][1], data[i][1] + data[i][3]):
-            mymap[k][j] += 1
+T = int(input())
+for case in range(1, T+1):
+    V, E = map(int,input().split())
 
-ans = [0] * num
-for i in range(num):
-    for j in range(data[i][0], data[i][0] + data[i][2]):
-        for k in range(data[i][1],data[i][1] +data[i][3]):
-            if mymap[k][j] in range(i+2):
-                ans[i] += 1
+    node = [[] for _ in range(V+1)]
 
-for i in range(num):
-    print(ans[i])
+    for i in range(E):
+        s, e = map(int,input().split())
+        node[s].append(e)
+
+
+    S, G = map(int,input().split())
+    TF = False
+    s = [S]
+    visited = [S]
+    while s:
+        current = s[-1]
+
+        for i in node[current]:
+            if i == G:
+                TF = True
+                break
+
+            elif i not in visited:
+                s.append(i)
+                visited.append(i)
+                break
+
+        else:
+            s.pop()
+
+        if TF:
+            break
+
+    if TF:
+        print("#%d"%(case), 1)
+    else:
+        print("#%d" % (case), 0)
