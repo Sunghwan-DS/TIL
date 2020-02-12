7d30abfd647d4a2dad08058dbd32e853aa81e2ba
sunghwan <jeonsung02@gmail.com>
2020-02-12 13:41:29 +0900

    시간 초과

--- BOJ_15684.py
+++ BOJ_15684.py
@@ -1,45 +1,75 @@
-def check(lst):
-    i = 0
-    for j in range(M):
+import sys
+sys.setrecursionlimit(10**6)
+
+def check(lst, res):
+    for j in range(N):
         x = j
-        while i != M:
-            for dir in range(2):
-                nx = j + LR[dir]
-                if 0 <= nx <= N-1:
-                    if lst[i][nx] = True:
-                        x = 
+        i = 0
+        while i < H:
+            if x-1 >= 0 and lst[i][x-1]:
+                x -= 1
+            elif x+1 <= N-1 and lst[i][x]:
+                x += 1
+            i += 1
+        if j == x:
+            pass
+        else:
+            return
+
+    print(res)
+    sys.exit()
 
 
-
-
-
-
-
-
-def order(idx, pre):
-    if idx == 3:
-        check(pre)
-    new = [pre[i][:] for i in range(M)]
-    for i in range(M):
+def order1(idx, pre):
+    if idx == 1:
+        check(pre, 1)
+        return
+    for i in range(H):
         for j in range(N-1):
-            if new[i][j] == True
+            if pre[i][j]:
                 continue
             else:
-                new[i][j] = True
-                order(idx+1, new)
-                new[i][j] = False
+                pre[i][j] = True
+                order1(idx+1, pre)
+                pre[i][j] = False
 
-LR = [-1, 0]
+def order2(idx, pre):
+    if idx == 2:
+        check(pre, 2)
+        return
+    for i in range(H):
+        for j in range(N-1):
+            if pre[i][j]:
+                continue
+            else:
+                pre[i][j] = True
+                order2(idx+1, pre)
+                pre[i][j] = False
 
-
-
-
+def order3(idx, pre):
+    if idx == 3:
+        check(pre, 3)
+        return
+    for i in range(H):
+        for j in range(N-1):
+            if pre[i][j]:
+                continue
+            else:
+                pre[i][j] = True
+                order3(idx+1, pre)
+                pre[i][j] = False
 
 
 N, M, H = map(int,input().split())
-arr = [[False] * (N-1) for _ in range(M)]
+arr = [[False] * (N-1) for _ in range(H)]
+
 for j in range(M):
-    a, b = map(int,input())
+    a, b = map(int,input().split())
     arr[a-1][b-1] = True
 
-order(0, arr)
+check(arr, 0)
+order1(0, arr)
+order2(0, arr)
+order3(0, arr)
+
+print(-1)
--- test.py
+++ test.py
@@ -1,4 +1,24 @@
-word = 'abcd'
+for case in range(1, 11):
+    N = int(input())
+    arr = [list(map(int,input().split())) for _ in range(N)]
+    ans = 0
+    for j in range(N):
+        i = 0
+        cnt = 0
+        while i < N:
+            if arr[i][j] == 1:
+                cnt += 1
+                s = 1
+                break
+            i += 1
 
-for idx, char in enumerate(word):
-    print(idx, char)
+        i += 1
+        while i < N:
+            if arr[i][j] != 0 and arr[i][j] != s:
+                cnt += 1
+                s = arr[i][j]
+            i += 1
+
+        ans += cnt//2
+
+    print("#%d"%(case), ans)
