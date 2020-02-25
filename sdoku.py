0a93d879784879bb7981fc9b9cbfc16d3329042b
Sunghwan-DS <jeonsung02@gmail.com>
2020-02-25 22:45:19 +0900

    2020.02.25

--- sdoku.py
+++ sdoku.py
@@ -1,89 +1,79 @@
-field_original = [list(map(int, input().split())) for _ in range(9)]
-check = [False] * 27    # 가로줄 9개, 세로줄 9게, 사각형 9개에 대한 체크리스트 27개
-line_check = [False] * 10    # 0자리도 포함하여 1부터 9까지 이미 사용한 숫자를 체크하기 위한 리스트
+import sys
+
+def row_check(y, x):
+    check = [False] * 10
+
+    for j in range(9):
+        if not check[arr[y][j]]:
+            if arr[y][j] != 0:
+                check[arr[y][j]] = True
+        else:
+            return False
+    return True
 
 
-def filling(index, field):
-    if index == 9:
-        for _ in range(9):
-            line_check 1
+def col_check(y, x):
+    check = [False] * 10
+
+    for i in range(9):
+        if not check[arr[i][x]]:
+            if arr[i][x] != 0:
+                check[arr[i][x]] = True
+        else:
+            return False
+    return True
 
 
-# while False in check:
-#     for i, horizontal_TF in enumerate(check[0:9]):    # horizontal_TF 는 n번 째 가로줄이 완성되있는가를 True, False로 표기
-#         if horizontal_TF:
-#             continue
-#
-#         if field[i].count(0) == 0:
-#             check[i] = True
-#             continue
-#
-#         if field[i].count(0) == 1:
-#             for _ in range(9):
-#                 line_check[field[i][_]] = True
-#
-#             field[i][field[i].index(0)] = line_check.index(False)
-#             check[i] = True
-#             line_check = [False] * 10
-#         else:
-#             pass
-#
-#     for i, verticle_TF in enumerate(check[9:18]):   # verticle_TF 는 n번 째 세로줄이 완성되있는가를 True, False로 표기
-#         if verticle_TF:
-#             continue
-#         count_zero = 0
-#         for _ in range(9):
-#             if field[_][i] == 0:
-#                 count_zero += 1
-#
-#         if count_zero == 0:
-#             check[i + 9] = True
-#             continue
-#
-#         if count_zero == 1:
-#             t = 0
-#             for _ in range(9):
-#                 line_check[field[_][i]] = True
-#                 if field[_][i] != 0:
-#                     t += 1
-#                 else:
-#                     tt = t
-#             field[tt][i] = line_check.index(False)
-#             check[i+9] = True
-#             line_check = [False] * 10
-#         else:
-#             pass
-#
-#     for i, square_TF in enumerate(check[18:]):    # square_TF 는 n번 째 3*3이 완성되있는가를 True, False로 표기
-#         if square_TF:
-#             continue
-#
-#
-#         count_zero = 0
-#         for y in range(i // 3 * 3, i // 3 * 3 + 3):
-#             for x in range(i % 3 * 3, i % 3 * 3 + 3):
-#                 if field[y][x] == 0:
-#                     count_zero += 1
-#
-#         if count_zero == 0:
-#             check[i + 18] = True
-#             continue
-#
-#         if count_zero == 1:
-#             t = 0
-#             for y in range(i // 3 * 3, i // 3 * 3 + 3):
-#                 for x in range(i % 3 * 3, i % 3 * 3 + 3):
-#                     line_check[field[y][x]] = True
-#                     if field[y][x] != 0:
-#                         t += 1
-#                     else:
-#                         tt = t
-#             field[i // 3 * 3 + tt // 3][i % 3 * 3 + tt % 3] = line_check.index(False)
-#             check[i + 18] = True
-#             line_check = [False] * 10
-#
-#         else:
-#             pass
+def square_check(y, x):
+    ny = y // 3
+    nx = x // 3
+    check = [False] * 10
 
-for _ in range(9):
-    print(" ".join([str(i) for i in field[_]]))
+    for i in range(3*ny, 3*ny+3):
+        for j in range(3*nx, 3*nx+3):
+            if not check[arr[i][j]]:
+                if arr[i][j] != 0:
+                    check[arr[i][j]] = True
+            else:
+                return False
+    return True
+
+
+def fill(idx):
+    global N
+    if idx == N:
+        for i in range(9):
+            print(*arr[i])
+        sys.exit()
+
+    y, x = zero[idx]
+    can_fill = [False] + [True] * 9
+
+    for k in range(9):
+        can_fill[arr[y][k]] = False
+        can_fill[arr[k][x]] = False
+
+    ny = y // 3 * 3
+    nx = x // 3 * 3
+
+    for i in range(ny, ny+3):
+        for j in range(nx, nx+3):
+            can_fill[arr[i][j]] = False
+    can_fill = [idx+1 for idx, i in enumerate(can_fill[1:]) if i]
+
+    for num in can_fill:
+        arr[y][x] = num
+        fill(idx+1)
+        arr[y][x] = 0
+
+
+arr = [list(map(int,input().split())) for _ in range(9)]
+zero = []
+
+for i in range(9):
+    for j in range(9):
+        if arr[i][j] == 0:
+            zero.append((i, j))
+
+N = len(zero)
+fill(0)
--- start_and_link.py
+++ start_and_link.py
@@ -1,9 +1,6 @@
 N = int(input())
 match = [list(map(int, input().split())) for _ in range(N)]
 team_set = {i for i in range(N)}
-total = 0
-for _ in range(N):
-    total += sum(match[_])
 answer = 1000
 
 
@@ -25,13 +22,12 @@ def make_teams(num, team):
     if num == N // 2:
         if answer > score(team):
             answer = score(team)
-        else:
-            return
+        return
 
     for i in range(max(team) + 1, N):
         team.append(i)
         make_teams(num + 1, team)
-        team.remove(i)
+        team.pop(-1)
 
 
 make_teams(1, [0])
