5567db39c2cfbec0061d3f4e9cc4cf7645045afd
Sunghwan-DS <jeonsung02@gmail.com>
2020-03-09 22:42:58 +0900

    2020.03.09

--- BOJ_13460.py
+++ BOJ_13460.py
@@ -1,61 +1,80 @@
 # 2020.03.09
 # 17:05 ~ 18:10
-# 시뮬레이션, DFS 구현
-# pypy:2156ms, 코드 길이:2323B
+# 시뮬레이션, BFS 구현
+# 시간:60ms, 코드 길이:3823B
 
-def DFS(Ry, Rx, By, Bx, idx):
-    global ans
-    if ans <= idx or idx == 11:
-        return
+def BFS(Ry, Rx, By, Bx):
+    q = [(Ry, Rx, By, Bx)]
+    visited[Ry][Rx][By][Bx] = True
+    cnt = 0
+    while q and cnt <= 10:
+        for i in range(len(q)):
+            Ry, Rx, By, Bx = q.pop(0)
+            if arr[Ry][Rx] == "O":
+                if arr[By][Bx] == "O":
+                    continue
+                else:
+                    print(cnt)
+                    exit()
 
-    if arr[Ry][Rx] == "O":
-        if arr[By][Bx] == "O":
-            return
-        else:
-            ans = min(ans, idx)
-            return
-
-    if arr[By][Bx] == "O":
-        return
-
-
-    if Ry >= By:
-        new_Ry, new_Rx = move(Ry, Rx, 2, By, Bx)
-        new_By, new_Bx = move(By, Bx, 2, new_Ry, new_Rx)
-        DFS(new_Ry, new_Rx, new_By, new_Bx, idx + 1)
-
-        new_By, new_Bx = move(By, Bx, 0, Ry, Rx)
-        new_Ry, new_Rx = move(Ry, Rx, 0, new_By, new_Bx)
-        DFS(new_Ry, new_Rx, new_By, new_Bx, idx + 1)
-
-    else:
-        new_By, new_Bx = move(By, Bx, 2, Ry, Rx)
-        new_Ry, new_Rx = move(Ry, Rx, 2, new_By, new_Bx)
-        DFS(new_Ry, new_Rx, new_By, new_Bx, idx + 1)
-
-        new_Ry, new_Rx = move(Ry, Rx, 0, By, Bx)
-        new_By, new_Bx = move(By, Bx, 0, new_Ry, new_Rx)
-        DFS(new_Ry, new_Rx, new_By, new_Bx, idx + 1)
+            if arr[By][Bx] == "O":
+                continue
 
 
-    if Rx >= Bx:
-        new_Ry, new_Rx = move(Ry, Rx, 1, By, Bx)
-        new_By, new_Bx = move(By, Bx, 1, new_Ry, new_Rx)
-        DFS(new_Ry, new_Rx, new_By, new_Bx, idx + 1)
+            if Ry >= By:
+                new_Ry, new_Rx = move(Ry, Rx, 2, By, Bx)
+                new_By, new_Bx = move(By, Bx, 2, new_Ry, new_Rx)
+                if not visited[new_Ry][new_Rx][new_By][new_Bx]:
+                    visited[new_Ry][new_Rx][new_By][new_Bx] = True
+                    q.append((new_Ry, new_Rx, new_By, new_Bx))
 
-        new_By, new_Bx = move(By, Bx, 3, Ry, Rx)
-        new_Ry, new_Rx = move(Ry, Rx, 3, new_By, new_Bx)
-        DFS(new_Ry, new_Rx, new_By, new_Bx, idx + 1)
+                new_By, new_Bx = move(By, Bx, 0, Ry, Rx)
+                new_Ry, new_Rx = move(Ry, Rx, 0, new_By, new_Bx)
+                if not visited[new_Ry][new_Rx][new_By][new_Bx]:
+                    visited[new_Ry][new_Rx][new_By][new_Bx] = True
+                    q.append((new_Ry, new_Rx, new_By, new_Bx))
 
-    else:
-        new_By, new_Bx = move(By, Bx, 1, Ry, Rx)
-        new_Ry, new_Rx = move(Ry, Rx, 1, new_By, new_Bx)
-        DFS(new_Ry, new_Rx, new_By, new_Bx, idx + 1)
+            else:
+                new_By, new_Bx = move(By, Bx, 2, Ry, Rx)
+                new_Ry, new_Rx = move(Ry, Rx, 2, new_By, new_Bx)
+                if not visited[new_Ry][new_Rx][new_By][new_Bx]:
+                    visited[new_Ry][new_Rx][new_By][new_Bx] = True
+                    q.append((new_Ry, new_Rx, new_By, new_Bx))
 
-        new_Ry, new_Rx = move(Ry, Rx, 3, By, Bx)
-        new_By, new_Bx = move(By, Bx, 3, new_Ry, new_Rx)
-        DFS(new_Ry, new_Rx, new_By, new_Bx, idx + 1)
+                new_Ry, new_Rx = move(Ry, Rx, 0, By, Bx)
+                new_By, new_Bx = move(By, Bx, 0, new_Ry, new_Rx)
+                if not visited[new_Ry][new_Rx][new_By][new_Bx]:
+                    visited[new_Ry][new_Rx][new_By][new_Bx] = True
+                    q.append((new_Ry, new_Rx, new_By, new_Bx))
 
+
+            if Rx >= Bx:
+                new_Ry, new_Rx = move(Ry, Rx, 1, By, Bx)
+                new_By, new_Bx = move(By, Bx, 1, new_Ry, new_Rx)
+                if not visited[new_Ry][new_Rx][new_By][new_Bx]:
+                    visited[new_Ry][new_Rx][new_By][new_Bx] = True
+                    q.append((new_Ry, new_Rx, new_By, new_Bx))
+
+                new_By, new_Bx = move(By, Bx, 3, Ry, Rx)
+                new_Ry, new_Rx = move(Ry, Rx, 3, new_By, new_Bx)
+                if not visited[new_Ry][new_Rx][new_By][new_Bx]:
+                    visited[new_Ry][new_Rx][new_By][new_Bx] = True
+                    q.append((new_Ry, new_Rx, new_By, new_Bx))
+
+            else:
+                new_By, new_Bx = move(By, Bx, 1, Ry, Rx)
+                new_Ry, new_Rx = move(Ry, Rx, 1, new_By, new_Bx)
+                if not visited[new_Ry][new_Rx][new_By][new_Bx]:
+                    visited[new_Ry][new_Rx][new_By][new_Bx] = True
+                    q.append((new_Ry, new_Rx, new_By, new_Bx))
+
+                new_Ry, new_Rx = move(Ry, Rx, 3, By, Bx)
+                new_By, new_Bx = move(By, Bx, 3, new_Ry, new_Rx)
+                if not visited[new_Ry][new_Rx][new_By][new_Bx]:
+                    visited[new_Ry][new_Rx][new_By][new_Bx] = True
+                    q.append((new_Ry, new_Rx, new_By, new_Bx))
+
+        cnt += 1
 
 def move(y, x, dir, other_y, other_x):
     ny = y + dy[dir]
@@ -71,6 +90,7 @@ def move(y, x, dir, other_y, other_x):
 
 dy = [-1, 0, 1, 0]
 dx = [0, 1, 0, -1]
+visited = [[[[False]*10 for _ in range(10)] for _ in range(10)] for _ in range(10)]
 
 N, M = map(int,input().split())
 arr = [list(input()) for _ in range(N)]
@@ -86,9 +106,5 @@ for i in range(N):
 arr[By][Bx] = "."
 arr[Ry][Rx] = "."
 
-ans = 20
-DFS(Ry, Rx, By, Bx, 0)
-if ans == 20:
-    print(-1)
-else:
-    print(ans)
+BFS(Ry, Rx, By, Bx)
+print(-1)
