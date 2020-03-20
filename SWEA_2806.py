66b56e305cb514e9114354ba01c0f6a2026d4595
Sunghwan-DS <jeonsung02@gmail.com>
2020-03-20 18:31:01 +0900

    2020.03.20

--- SWEA_2806.py
+++ SWEA_2806.py
@@ -1,19 +1,34 @@
-def go(idx, visited):
+def go(idx):
     global N, ans
     if idx == N:
         ans += 1
         return
 
     for i in range(N):
-        if
+        if not row_visited[i]:
+            for j in range(idx):
+                if idx - j == abs(i - col[j]):
+                    break
+
+            else:
+                row_visited[i] = True
+                col[idx] = i
+                go(idx+1)
+                row_visited[i] = False
+                col[idx] = 0
 
 
 for tc in range(1, int(input()) + 1):
     N = int(input())
-    col_visited = [0] * N
+    row_visited = [False] * N
+    col = [0] * N
 
     ans = 0
     for i in range(N):
-        col_visited[i] = True
-        go(1, col_visited)
-        col_visited[i] = False
+        row_visited[i] = True
+        col[0] = i
+        go(1)
+        row_visited[i] = False
+        col[0] = 0
+
+    print("#%d"%(tc), ans)
