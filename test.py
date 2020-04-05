968bed2bd577e0f6073d12226c90dcf4233bce75
Sunghwan-DS <jeonsung02@gmail.com>
2020-04-05 23:47:19 +0900

    2020.04.05

--- test.py
+++ test.py
@@ -6,5 +6,13 @@ for i in range(N-1):
     connect[b].append(a)
 
 parent = [0] * (N+1)
-parents = [1]
-for i in connect[1]:
+s = [1]
+while s:
+    p = s.pop()
+    for i in connect[p]:
+        if not parent[i]:
+            parent[i] = p
+            s.append(i)
+
+for i in range(2, N+1):
+    print(parent[i])
