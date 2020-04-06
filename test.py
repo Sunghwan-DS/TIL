4117e4e48b60170358b7cf77c569eeac57542382
Sunghwan-DS <jeonsung02@gmail.com>
2020-04-06 18:21:00 +0900

    2020.04.06

--- test.py
+++ test.py
@@ -1,18 +1,10 @@
 N = int(input())
-connect = [[] for _ in range(N+1)]
-for i in range(N-1):
-    a, b = map(int,input().split())
-    connect[a].append(b)
-    connect[b].append(a)
+arr = [set() for _ in range(51)]
+for i in range(N):
+    c = input()
+    arr[len(c)].add(c)
 
-parent = [0] * (N+1)
-s = [1]
-while s:
-    p = s.pop()
-    for i in connect[p]:
-        if not parent[i]:
-            parent[i] = p
-            s.append(i)
-
-for i in range(2, N+1):
-    print(parent[i])
+for i in range(1, 51):
+    if arr[i]:
+        for word in sorted(list(arr[i])):
+            print(word)
