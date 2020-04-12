21439025f17eb28a98f75427454e635677c0220f
Sunghwan-DS <jeonsung02@gmail.com>
2020-04-12 22:59:31 +0900

    2020.04.12

--- Python/BOJ/BOJ_1949.py
+++ Python/BOJ/BOJ_1949.py
@@ -1,45 +1,66 @@
-class L_Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+# class L_Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+#
+#
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None
+#         self.tail = None
+#
+#     def append(self, data):
+#         new = L_Node(data)
+#         if self.head == None:
+#             self.head = new
+#             self.tail = new
+#
+#         else:
+#             self.tail.next = new
+#             self.tail = new
+#
+#
+# class T_Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.parent = None
+#         self.child = LinkedList()
+#         self.current = None
+#
+#
+# class Tree:
+#     def __init__(self):
+#         self.root = None
+#         self.current = None
+#
+#     def add(self, data):
+#         new = T_Node(data)
+#         if self.root == None:
+#             self.root = new
+#         else:
+#             self.current.child.append(new)
+#             new.parent = self.current
 
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def append(self, data):
-        new = L_Node(data)
-        if self.head == None:
-            self.head = new
-            self.tail = new
+def DFS(current):
+    visited[current] = True
+    s = [current]
+    DP[current][0] = population[current]
+    while s:
+        current = s[-1]
+        for next in connection[current]:
+            if not visited[next]:
+                s.append(next)
+                visited[next] = True
+                DP[next][0] = population[next]
+                break
 
         else:
-            self.tail.next = new
-            self.tail = new
+            s.pop()
+            if s:
+                DP[s[-1]][0] += DP[current][1]
+                DP[s[-1]][1] += max(DP[current][0], DP[current][1])
 
-
-class T_Node:
-    def __init__(self, data):
-        self.data = data
-        self.parent = None
-        self.child = LinkedList()
-        self.current = None
-
-
-class Tree:
-    def __init__(self):
-        self.root = None
-        self.current = None
-
-    def add(self, data):
-        new = T_Node(data)
-        if self.root == None:
-            self.root = new
-        else:
-            self.current.child.append(new)
-            new.parent = self.current
 
 N = int(input())
 population = [0] + list(map(int,input().split()))
@@ -49,9 +70,7 @@ for i in range(N-1):
     connection[a].append(b)
     connection[b].append(a)
 
-
-tree = Tree()
-tree.add(1)
-
-for child in connection[1]:
-    if child !=
+DP = [[0, 0] for _ in range(N+1)]
+visited = [False] * (N+1)
+DFS(1)
+print(max(DP[1][0], DP[1][1]))
